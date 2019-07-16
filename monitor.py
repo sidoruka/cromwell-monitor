f89aeed0b5947454b2cbe44256baeaf8fdb14f5a
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2019-07-16 14:44:25 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from functools import reduce
-from googleapiclient.discovery import build as google_api
 from google.cloud.monitoring_v3 import MetricServiceClient
 from google.cloud.monitoring_v3.types import LabelDescriptor, MetricDescriptor, TimeSeries
-import json
 from os import environ
 import psutil as ps
 import requests
@@ -12,84 +10,14 @@ from signal import signal, SIGTERM
 from sys import stderr
 from time import sleep, time
 
-compute = google_api('compute', 'v1')
-
-def get_machine_info():
-  metadata = requests.get(
-    'http://metadata.google.internal/computeMetadata/v1/instance/?recursive=true',
+def get_metadata(key):
+  return requests.get(
+    'http://metadata.google.internal/computeMetadata/v1/instance/' + key,
     headers={'Metadata-Flavor': 'Google'}
-  ).json()
-
-  name = metadata['name']
-  _, project, _, zone = metadata['zone'].split('/')
-  instance = compute.instances().get(project=project, zone=zone, instance=name).execute()
-
-  disks = [get_disk(project, zone, disk) for disk in instance['disks']]
-
-  return {
-    'project': project,
-    'zone': zone,
-    'region': zone[:-2],
-    'name': name,
-    'type': instance['machineType'].split('/')[-1],
-    'preemptible': instance['scheduling']['preemptible'],
-    'disks': disks,
-  }
-
-def get_disk(project, zone, disk):
-  if disk['type'] == 'PERSISTENT':
-    name = disk['source'].split('/')[-1]
-    resource = compute.disks().get(project=project, zone=zone, disk=name).execute()
-    return {
-      'type': resource['type'].split('/')[-1],
-      'sizeGb': int(resource['sizeGb']),
-    }
-  else:
-    return {
-      'type': 'local-ssd',
-      'sizeGb': 375,
-    }
-
-PRICELIST_JSON = 'pricelist.json'
-
-def get_pricelist():
-  with open(PRICELIST_JSON, 'r') as f:
-    return json.load(f)['gcp_price_list']
-
-def get_price_key(key, preemptible):
-  return 'CP-COMPUTEENGINE-' + key + ('-PREEMPTIBLE' if preemptible else '')
-
-def get_machine_hour(machine, pricelist):
-  if machine['type'].startswith('custom'):
-    _, core, memory = machine['type'].split('-')
-    core_key = get_price_key('CUSTOM-VM-CORE', machine['preemptible'])
-    memory_key = get_price_key('CUSTOM-VM-RAM', machine['preemptible'])
-    return pricelist[core_key][machine['region']] * int(core) + \
-      pricelist[memory_key][machine['region']] * int(memory) / 2**10
-  else:
-    price_key = get_price_key('VMIMAGE-' + machine['type'].upper(), machine['preemptible'])
-    return pricelist[price_key][machine['region']]
-
-def get_disk_hour(machine, pricelist):
-  total = 0
-  for disk in machine['disks']:
-    price_key = 'CP-COMPUTEENGINE-'
-    if disk['type'] == 'pd-standard':
-      price_key += 'STORAGE-PD-CAPACITY'
-    elif disk['type'] == 'pd-ssd':
-      price_key += 'STORAGE-PD-SSD'
-    elif disk['type'] == 'local-ssd':
-      price_key += 'LOCAL-SSD'
-      if machine['preemptible']:
-        price_key += '-PREEMPTIBLE'
-    price = pricelist[price_key][machine['region']] * disk['sizeGb']
-    if disk['type'].startswith('pd'):
-      price /= 730 # hours per month
-    total += price
-  return total
+  ).text
 
 def reset():
-  global memory_used, disk_used, disk_reads, disk_writes, last_time
+  global memory_used, disk_used, disk_reads, disk_writes, report_time
 
   # Explicitly reset the CPU counter, because the first call of this method always reports 0
   ps.cpu_percent()
@@ -100,14 +28,15 @@ def reset():
   disk_reads = disk_io('read_count')
   disk_writes = disk_io('write_count')
 
-  last_time = time()
+  report_time = 0
 
 def measure():
-  global memory_used, disk_used
+  global memory_used, disk_used, report_time
 
   memory_used = max(memory_used, MEMORY_SIZE - mem_usage('available'))
   disk_used = max(disk_used, disk_usage('used'))
 
+  report_time += MEASUREMENT_TIME_SEC
   sleep(MEASUREMENT_TIME_SEC)
 
 def mem_usage(param):
@@ -139,7 +68,6 @@ def create_time_series(series):
   client.create_time_series(PROJECT_NAME, series)
 
 def get_time_series(metric_descriptor, value):
-  global last_time
   series = TimeSeries()
 
   series.metric.type = metric_descriptor.type
@@ -151,28 +79,23 @@ def get_time_series(metric_descriptor, value):
   labels['cpu_count'] = CPU_COUNT_LABEL
   labels['mem_size'] = MEMORY_SIZE_LABEL
   labels['disk_size'] = DISK_SIZE_LABEL
-  labels['preemptible'] = PREEMPTIBLE_LABEL
 
   series.resource.type = 'gce_instance'
-  series.resource.labels['zone'] = MACHINE['zone']
-  series.resource.labels['instance_id'] = MACHINE['name']
+  series.resource.labels['zone'] = ZONE
+  series.resource.labels['instance_id'] = INSTANCE
 
   point = series.points.add(value=value)
-  end_time = max(time(), last_time + REPORT_TIME_SEC_MIN)
-  point.interval.end_time.seconds = int(end_time)
+  point.interval.end_time.seconds = int(time())
 
   return series
 
 def report():
-  global last_time
-  time_delta = time() - last_time
   create_time_series([
     get_time_series(CPU_UTILIZATION_METRIC, { 'double_value': ps.cpu_percent() }),
     get_time_series(MEMORY_UTILIZATION_METRIC, { 'double_value': memory_used / MEMORY_SIZE * 100 }),
     get_time_series(DISK_UTILIZATION_METRIC, { 'double_value': disk_used / DISK_SIZE * 100 }),
-    get_time_series(DISK_READS_METRIC, { 'double_value': (disk_io('read_count') - disk_reads) / time_delta }),
-    get_time_series(DISK_WRITES_METRIC, { 'double_value': (disk_io('write_count') - disk_writes) / time_delta }),
-    get_time_series(COST_ESTIMATE_METRIC, { 'double_value': (time() - ps.boot_time()) * COST_PER_SEC }),
+    get_time_series(DISK_READS_METRIC, { 'double_value': (disk_io('read_count') - disk_reads) / report_time }),
+    get_time_series(DISK_WRITES_METRIC, { 'double_value': (disk_io('write_count') - disk_writes) / report_time }),
   ])
 
 ### Define constants
@@ -185,19 +108,18 @@ TASK_CALL_INDEX = environ['TASK_CALL_INDEX']
 TASK_CALL_ATTEMPT = environ['TASK_CALL_ATTEMPT']
 DISK_MOUNTS = environ['DISK_MOUNTS'].split()
 
-# Get billing rates
-MACHINE = get_machine_info()
-PRICELIST = get_pricelist()
-COST_PER_SEC = (get_machine_hour(MACHINE, PRICELIST) + get_disk_hour(MACHINE, PRICELIST)) / 3600
+# GCP instance name, zone and project
+# from instance introspection API
+INSTANCE = get_metadata('name')
+_, PROJECT, _, ZONE = get_metadata('zone').split('/')
 
 client = MetricServiceClient()
-PROJECT_NAME = client.project_path(MACHINE['project'])
+PROJECT_NAME = client.project_path(PROJECT)
 
 METRIC_ROOT = 'wdl_task'
 
 MEASUREMENT_TIME_SEC = 1
-REPORT_TIME_SEC_MIN = 60
-REPORT_TIME_SEC = REPORT_TIME_SEC_MIN
+REPORT_TIME_SEC = 60
 
 LABEL_DESCRIPTORS = [
   LabelDescriptor(
@@ -228,10 +150,6 @@ LABEL_DESCRIPTORS = [
     key='disk_size',
     description='Total disk size, GB',
   ),
-  LabelDescriptor(
-    key='preemptible',
-    description='Preemptible flag',
-  ),
 ]
 
 CPU_COUNT = ps.cpu_count()
@@ -242,8 +160,6 @@ MEMORY_SIZE_LABEL = format_gb(MEMORY_SIZE)
 
 DISK_SIZE = disk_usage('total')
 DISK_SIZE_LABEL = format_gb(DISK_SIZE)
-
-PREEMPTIBLE_LABEL = str(MACHINE['preemptible']).lower()
 
 CPU_UTILIZATION_METRIC = get_metric(
   'cpu_utilization', 'DOUBLE', '%',
@@ -270,11 +186,6 @@ DISK_WRITES_METRIC = get_metric(
   'Disk write IOPS in a Cromwell task call',
 )
 
-COST_ESTIMATE_METRIC = get_metric(
-  'runtime_cost_estimate', 'DOUBLE', 'USD',
-  'Cumulative runtime cost estimate for a Cromwell task call',
-)
-
 ### Detect container termination
 
 def signal_handler(signum, frame):
@@ -296,7 +207,7 @@ signal(SIGTERM, signal_handler)
 reset()
 while running:
   measure()
-  if (time() - last_time) >= REPORT_TIME_SEC:
+  if report_time >= REPORT_TIME_SEC:
     report()
     reset()
 exit(0)
